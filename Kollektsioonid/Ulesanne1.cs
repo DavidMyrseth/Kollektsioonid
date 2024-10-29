6149080b255e5f0a8c1bc7a2d545ad07dc08c04f
DavidMyrseth <153905309+DavidMyrseth@users.noreply.github.com>
2024-10-29 15:24:58 +0200

    Update Ulesanne1.cs

--- Kollektsioonid/Ulesanne1.cs
+++ Kollektsioonid/Ulesanne1.cs
@@ -31,5 +31,102 @@
             // Вывод результата в строку
             Console.WriteLine(string.Join(" ", result));
         }
+
+        public static void Terveitamine()
+        {
+            Console.WriteLine("СДЕЛАНО ЧЕРЕЗ ЦИКЛ FOR");
+            for (int i = 0; i < 18; i++)
+            {
+                Console.WriteLine("Привет Андрей");
+            }
+
+            Console.WriteLine("СДЕЛАНО ЧЕРЕЗ ЦИКЛ DO WHILE");
+            int j = 0;
+            do
+            {
+                Console.WriteLine("Привет Андрей");
+                j++;
+            } while (j < 18);
+
+            Console.WriteLine("СДЕЛАНО ЧЕРЕЗ ЦИКЛ foreach");
+            var greetings = new string[18];
+            foreach (var _ in greetings)
+            {
+                Console.WriteLine("Привет Андрей");
+            }
+        }
+
+        public void Kommi()
+        {
+            Console.WriteLine("Попрошайка");
+
+            Random random = new Random();
+            int number = random.Next(1, 11);
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine($"Дай конфетку, пожалуйста {i}!");
+            }
+
+            Console.WriteLine("Торгующийся");
+
+            int current = number;
+
+            while (current > 0)
+            {
+                Console.WriteLine($"Ну дай, пожалуйста, конфетку {current}?");
+                string response = Console.ReadLine().ToLower();
+
+                if (response == "yes")
+                {
+                    Console.WriteLine("Спасибо! Ты лучший!");
+                    return;
+                }
+                else if (response == "no")
+                {
+                    Console.WriteLine("Почему? Я так хочу конфетку");
+                    current--;
+                }
+            }
+
+            // Прощания
+            for (int i = current; i >= 0; i--)
+            {
+                Console.WriteLine($"Прощай, {i}!");
+            }
+        }
+
+        public static void Vikto()
+        {
+            Console.Write("Введите начало диапазона: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("Введите конец диапазона: ");
+            int end = int.Parse(Console.ReadLine());
+
+            if (start >= end)
+            {
+                Console.WriteLine("Ошибка: начало диапазона должно быть меньше конца");
+                return;
+            }
+
+            int numberToGuess = new Random().Next(start, end + 1);
+            Console.WriteLine($"Я задумал число от {start} до {end}. Попробуй угадать!");
+
+            while (true)
+            {
+                Console.Write("Введите ваше предположение: ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess < numberToGuess)
+                    Console.WriteLine("Слишком мало! Попробуй еще раз");
+                else if (guess > numberToGuess)
+                    Console.WriteLine("Слишком много! Попробуй еще раз");
+                else
+                {
+                    Console.WriteLine($"Поздравляю! Вы угадали число {numberToGuess}");
+                    break;
+                }
+            }
+        }
     }
 }
